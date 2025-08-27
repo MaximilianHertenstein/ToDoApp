07d4ecd286244dfe752a664df48b0ce3e4a59c6b
Max <max.hertenstein@gmx.de>
2025-08-27 12:14:30 +0200

    update

--- src/main/java/org/example/ServerView.java
+++ src/main/java/org/example/ServerView.java
@@ -1,33 +1,4 @@
 package org.example;
 
-import io.javalin.http.Context;
-
-import java.util.List;
-import java.util.Map;
-
-public class ServerView {
-
-
-
-    public void renderApp(Context ctx, List<ToDo> toDos, String countOfActiveToDosDisplay, String appliedFilter) {
-        var map = Map.of("toDos", toDos, "countOfActiveToDosDisplay", countOfActiveToDosDisplay, "appliedFilter", appliedFilter);
-        var templateName = "mainPage.jte";
-        if (ctx.header("HX-Request") != null) {
-            templateName = "app.jte";
-        }
-
-        ctx.render(templateName, map);
-
-    }
-
-
-    public void renderEditForm(Context ctx, ToDo toDo) {
-        ctx.render("editingForm.jte", Map.of("toDo", toDo));
-    }
-
-
-    public void renderToDo(Context ctx, ToDo toDo) {
-        ctx.render("singleItem.jte", Map.of("toDo", toDo));
-    }
-
+public interface ServerView {
 }
