5748d108eda85d2752c379253d72ba92df83fd3b
Max <max.hertenstein@gmx.de>
2025-09-01 19:43:20 +0200

    update

---
+++ src/test/java/org/example/ToDoTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ToDoTest {
+
+    @Test
+    void toggleDone() {
+    }
+
+    @Test
+    void updateText() {
+    }
+
+    @Test
+    void id() {
+    }
+
+    @Test
+    void text() {
+    }
+
+    @Test
+    void completed() {
+    }
+}
