20f66b36eb84adf1db157b88005f46a5b1070750
Max <max.hertenstein@gmx.de>
2025-09-11 08:45:59 +0200

    update

--- src/main/java/org/example/ServerMain.java
+++ src/main/java/org/example/ServerMain.java
@@ -1,15 +1,6 @@
 package org.example;
 
-import gg.jte.ContentType;
-import gg.jte.TemplateEngine;
-import gg.jte.output.StringOutput;
-import gg.jte.resolve.DirectoryCodeResolver;
 import io.javalin.Javalin;
-import io.javalin.config.JavalinConfig;
-import io.javalin.plugin.bundled.CorsPluginConfig;
-import org.example.*;
-
-import java.util.List;
 
 import static java.io.IO.println;
 
@@ -21,26 +12,31 @@ class ServerMain {
     public static void main(String[] args) {
 
 
+        runApp();
 
-        var serverView = new ServerView();
-        var uiState = new UIState("Completed", List.of(), "0 items left");
-        println(serverView.renderToString("mainPage", uiState, false));
+//        var templateRenderer = new TemplateRenderer();
+//        ToDo learnJava = new ToDo(2, "Learn Java", false);
+//        println(templateRenderer.renderToDoToString(learnJava, false,false));
 
     }
 
 
 
-    void runApp() {
+    static void runApp() {
+
+
         var app =  Javalin.create(Utils::configureJavalin);
+
+//        var app =  Javalin.create(Utils::configureJavalin);
         var serverController = new ServerController();
-        app.get("/todos", serverController::renderApp);
+        app.get("/todos", serverController::showApp);
         app.post("/todos/new", serverController::addToDo);
         app.post("/todos/{id}/toggleStatus", serverController::toggleStatus);
         app.post("/todos/setFilter/{filter}", serverController::setFilter);
-        app.get("/todos/{id}/edit", serverController::editToDo);
+        app.get("/todos/{id}/edit", serverController::showEditForm);
         app.post("/todos/{id}/edit", serverController::updateTextOfToDo);
         app.post("/todos/{id}/delete", serverController::deleteToDo);
-        app.post("/todos/clearCompleted", serverController::deleteCompletedToDos);
+        app.post("/todos/clearCompletedToDos", serverController::clearCompletedToDos);
         app.start();
     }
 }
@@ -49,7 +45,7 @@ class ServerMain {
 //    var serverController = new ServerController();
 //    var webApp = Javalin.create(Utils::configureJavalin);
 //    hostCommonRoutes(webApp, serverController);
-//    webApp.delete("/todos/completed", serverController::deleteCompletedToDos);
+//    webApp.delete("/todos/completed", serverController::clearCompletedToDos);
 //    webApp.delete("/todos/{id}", serverController::deleteToDo);
 //    webApp.start();
 //}
@@ -59,6 +55,6 @@ class ServerMain {
 //    var serverController = new ServerController();
 //    hostCommonRoutes(mobileApp, serverController);
 //    mobileApp.post("/todos/{id}/delete", serverController::deleteToDo);
-//    mobileApp.post("/todos/clearCompleted", serverController::deleteCompletedToDos);
+//    mobileApp.post("/todos/clearCompleted", serverController::clearCompletedToDos);
 //    mobileApp.start();
 //}
--- src/main/java/org/example/Utils.java
+++ src/main/java/org/example/Utils.java
@@ -1,26 +1,32 @@
 package org.example;
 
 
+import io.javalin.Javalin;
 import io.javalin.config.JavalinConfig;
 import io.javalin.plugin.bundled.CorsPluginConfig;
 
 
 
 public class Utils {
+//    public static void configureJavalin(JavalinConfig javalinConfig) {
+//
+//        javalinConfig.bundledPlugins.enableCors(cors -> {
+//            //it.allowHost("http://localhost:19006");
+//            cors.addRule(CorsPluginConfig.CorsRule::anyHost);
+//        });
+//        javalinConfig.staticFiles.enableWebjars();
+//
+//
+//
+//        //javalinConfig.fileRenderer(new JavalinJte());
+//
+//
+//    }
+
     public static void configureJavalin(JavalinConfig javalinConfig) {
-
-        javalinConfig.bundledPlugins.enableCors(cors -> {
-            //it.allowHost("http://localhost:19006");
-            cors.addRule(CorsPluginConfig.CorsRule::anyHost);
-        });
         javalinConfig.staticFiles.enableWebjars();
-
-
-
-        //javalinConfig.fileRenderer(new JavalinJte());
-
-
     }
+
 
 
 
