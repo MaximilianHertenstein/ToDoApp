242c5d0009e926c0886685ba268f1ae5b04086bb
Max <max.hertenstein@gmx.de>
2025-09-11 08:48:21 +0200

    update

--- src/main/java/org/example/main.java
+++ src/main/java/org/example/main.java
@@ -1,12 +1,6 @@
 package org.example;
 
-import org.example.TerminalController;
-import java.util.List;
 
-
-
-import org.example.ToDo;
-import org.example.View;
 
 class Main {
     public static void main() {
