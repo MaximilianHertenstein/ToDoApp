cc0918710887dbbe7e75f5d0ed7b30e714806906
MaximilianHertenstein <hert@ghse.de>
2026-03-04 21:07:54 +0100

    Javalin 7.0.1

--- src/main/java/org/example/JavalinConfigurator.java
+++ src/main/java/org/example/JavalinConfigurator.java
@@ -1,4 +1,19 @@
 package org.example;
 
+import io.javalin.config.JavalinConfig;
+
 public class JavalinConfigurator {
+
+    public static void configureJavalin(JavalinConfig javalinConfig) {
+        var serverController = new ServerController();
+        javalinConfig.routes.get("/todos", serverController::showApp);
+        javalinConfig.routes.post("/todos/new", serverController::addToDo);
+        javalinConfig.routes.post("/todos/{id}/toggleStatus", serverController::toggleStatus);
+        javalinConfig.routes.post("/todos/setFilter/{filter}", serverController::setFilter);
+        javalinConfig.routes.get("/todos/{id}/edit", serverController::showEditForm);
+        javalinConfig.routes.post("/todos/{id}/edit", serverController::updateTextOfToDo);
+        javalinConfig.routes.post("/todos/{id}/delete", serverController::deleteToDo);
+        javalinConfig.routes.post("/todos/clearCompletedToDos", serverController::clearCompletedToDos);
+        javalinConfig.staticFiles.enableWebjars();
+    }
 }
