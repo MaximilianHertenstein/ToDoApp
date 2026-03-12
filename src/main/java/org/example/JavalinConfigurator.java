54ea8f18705f52536f5e3f9f510fb887342b892d
MaximilianHertenstein <hert@ghse.de>
2026-03-12 16:42:04 +0100

    update

--- src/main/java/org/example/JavalinConfigurator.java
+++ src/main/java/org/example/JavalinConfigurator.java
@@ -1,19 +1,27 @@
 package org.example;
 
 import io.javalin.config.JavalinConfig;
+import io.javalin.plugin.bundled.CorsPluginConfig;
 
 public class JavalinConfigurator {
 
     public static void configureJavalin(JavalinConfig javalinConfig) {
         var serverController = new ServerController();
-        javalinConfig.routes.get("/todos", serverController::showApp);
-        javalinConfig.routes.post("/todos/new", serverController::addToDo);
-        javalinConfig.routes.post("/todos/{id}/toggleStatus", serverController::toggleStatus);
-        javalinConfig.routes.post("/todos/setFilter/{filter}", serverController::setFilter);
-        javalinConfig.routes.get("/todos/{id}/edit", serverController::showEditForm);
-        javalinConfig.routes.post("/todos/{id}/edit", serverController::updateTextOfToDo);
-        javalinConfig.routes.post("/todos/{id}/delete", serverController::deleteToDo);
-        javalinConfig.routes.post("/todos/clearCompletedToDos", serverController::clearCompletedToDos);
+        var routes = javalinConfig.routes;
+        routes.get("/todos", serverController::showApp);
+        routes.post("/todos/new", serverController::addToDo);
+        routes.post("/todos/{id}/toggleStatus", serverController::toggleStatus);
+        routes.post("/todos/setFilter/{filter}", serverController::setFilter);
+        routes.get("/todos/{id}/edit", serverController::showEditForm);
+        routes.post("/todos/{id}/edit", serverController::updateTextOfToDo);
+        routes.post("/todos/{id}/delete", serverController::deleteToDo);
+        routes.post("/todos/clearCompletedToDos", serverController::clearCompletedToDos);
         javalinConfig.staticFiles.enableWebjars();
+                javalinConfig.bundledPlugins.enableCors(cors -> {
+            //cors.       allowHost("http://localhost:19006");
+            cors.addRule(CorsPluginConfig.CorsRule::anyHost);
+        });
+
+
     }
 }
